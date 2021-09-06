e46cd80bfa03017af3784bf4714ffeec72334f9b
Andres F Morales A <anfelpe.0200@gmail.com>
2021-09-06 17:26:24 -0500

    Second version for API streaming

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
-from api.views import NodeInformation
+from api.views import WheaterInformation
 from django.urls import path
 
 urlpatterns = [
     #path('transactions_document/update/<uuid:pk>', TransactionsDocumentUpdateAPIView.as_view(), name=TransactionsDocumentUpdateAPIView.name)
-    path('v1/node_information/', NodeInformation.as_view(), name="NodeInformation"),
+    path('v1/obtain_weather_info/', WheaterInformation.as_view(), name="WheaterInformation"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,33 +1,92 @@
+import requests
+import json
 from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 
-import json
 # Create your views here.
-class NodeInformation(APIView):
+class WheaterInformation(APIView):
 
     #TODO get params by GET, but using regex
     def post(self, request, format=None):
-        import random
+        lat= request.POST['latitud']
+        lon= request.POST['longitud']
 
-        number= random.randrange(10)
-        response= ''
-        if number % 3 == 0:
-            response= 'Esta lloviendo'
+        latitud= float(lat)
+        longitud= float(lon)
+        node= ''
+
+        if (latitud >= 4.682108476725629) and (latitud <= 4.699181503183543):
+            node= 1
         
-        elif number % 3 == 1:
-            response= 'Esta haciendo sol'
+        elif (latitud >= 4.699181503183544) and (latitud <= 4.70136090941223):
+            node= 2
+
+        elif (latitud >= 4.70136090941223) and (latitud <= 4.737225049017668):
+            node= 3
+
+        elif (latitud >= 4.737225049017668) and (latitud <= 4.767225049017668):
+            node= 4
 
         else:
-            response= 'Esta haciendo mucha neblina'
+            node= None
+        
+        response= {}
+        if node != None:
+            response['Connection message']= f'Enlazando al nodo {node}. '
+            response = self.queryAPI(node, response, 1)
 
+        else:
+            response['Response']= 'No hay cobertura en este sitio'
+            response['Status code']= 400
        
         return Response(response, status=status.HTTP_200_OK)
 
     def get(self, request, format=None):
             
-        response= 'El nodo esta activo'
+        response= 'Prueba'
         return Response(response, status=status.HTTP_200_OK)
 
+
+    def queryAPI(self, node, response, contador):
+        web = '18.188.128.120'
+
+        if node == 1:
+            url = f'http://{web}:3000/api/v1/node_information/' 
+        elif node == 2:
+            url = f'http://{web}:4000/api/v1/node_information/' 
+        elif node == 3:
+            url = f'http://{web}:5000/api/v1/node_information/' 
+        elif node == 4:
+            url = f'http://{web}:6000/api/v1/node_information/' 
+        
+        print('*'*20)
+        print(f'Enlazando al nodo {node}')
+        print(url)        
+        print('*'*20)
+
+        try:
+            res = requests.request('POST', url, verify= False)
+            response['Response']= res.json()
+            response['Status code']= res.status_code
+            response['Nodo']= node
+
+        except:
+            response['Connection message']+= f'El nodo {node} se encuentra caido. '
+            if contador > 4:
+                response['Connection message']= 'Todos los nodos se encuentran caidos. '
+                response['Status code']= 500
+                response['Nodo']= 0
+            else:
+                if (node+1) > 4:
+                    node= 1
+                else:
+                    node+= 1
+                response['Connection message']+= f'Enlazando al nodo {node}. '
+                response= self.queryAPI(node,response, contador+1)
+
+        return response
+
+        
